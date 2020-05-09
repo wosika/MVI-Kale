7c68e6644912a8e1cdbbf3ae1e935bff0e2e4c13
sunze <wosikasun@gmail.com>
2020-05-09 17:06:25 +0800

    一个mvi模式添加。

--- app/src/main/java/com/wosika/kale/demo/MainActivity.kt
+++ app/src/main/java/com/wosika/kale/demo/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.wosika.kale.demo
 
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 import com.wosika.kale.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : BaseActivity<MainViewModel, MainViewState, MainIntent>() {
@@ -13,12 +16,36 @@ class MainActivity : BaseActivity<MainViewModel, MainViewState, MainIntent>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binds()
         viewModel.intent(MainIntent.InitIntent)
+    }
+
+    private fun binds() {
+        btnRetry.setOnClickListener {
+            viewModel.intent(MainIntent.InitIntent)
+        }
+
+        srlRefresh.setOnRefreshListener {
+            viewModel.intent(MainIntent.InitIntent)
+        }
     }
 
 
     override fun render(viewState: MainViewState) {
+        srlRefresh.isRefreshing = viewState.isRefresh
+        if (!viewState.data.isNullOrBlank()) {
+            tvContent.text = viewState.data
+            tvContent.visibility = View.VISIBLE
+        } else {
+            tvContent.visibility = View.GONE
+        }
 
+        if (viewState.error != null) {
+            Snackbar.make(srlRefresh, "出现错误", Snackbar.LENGTH_SHORT).show()
+            btnRetry.visibility = View.VISIBLE
+        } else {
+            btnRetry.visibility = View.GONE
+        }
     }
 
 
--- app/src/main/java/com/wosika/kale/demo/MainViewState.kt
+++ app/src/main/java/com/wosika/kale/demo/MainViewState.kt
@@ -2,4 +2,4 @@ package com.wosika.kale.demo
 
 import com.wosika.kale.viewstate.IViewState
 
-data class MainViewState(val data: String) : IViewState
+data class MainViewState(val isRefresh: Boolean, val data: String?, val error: Exception?) : IViewState
