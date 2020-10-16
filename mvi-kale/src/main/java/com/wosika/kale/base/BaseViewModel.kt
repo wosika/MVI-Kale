94b142f3ac9db76cc3afbcdb27fb1f7af2e4731a
wosika <wosikakale@gmail.com>
2020-10-16 17:00:21 +0800

    添加addViewState方法，区分postViewState。

--- mvi-kale/src/main/java/com/wosika/kale/base/BaseViewModel.kt
+++ mvi-kale/src/main/java/com/wosika/kale/base/BaseViewModel.kt
@@ -10,15 +10,29 @@ import com.wosika.kale.viewmodel.IViewModel
 import com.wosika.kale.viewstate.IViewState
 import kotlinx.coroutines.*
 
-
+//todo 不应该将viewStateLiveData限制为MutableLiveData 还需要优化。
 abstract class BaseViewModel<VS : IViewState, I : IIntent> : ViewModel(), IViewModel<VS, I> {
     override fun viewStateObservable(): LiveData<VS> = viewStateLiveData
 
-    //增加直接发送viewState的方法
+
+    /**
+     * "使用postViewState在频繁刷新数据时有可能丢失,如果为了保证数据能够传送过去请使用addViewState"
+     * @param viewState VS 需要发送给view层的viewState
+     */
     protected fun postViewState(viewState: VS) {
-        //todo 不应该将viewStateLiveData限制为MutableLiveData 还需要优化。
         viewStateLiveData.postValue(viewState)
     }
+
+    /**
+     * liveData中的setValue只能在主线程中使用,所以使用协程切换主线程后再调用
+     * @param viewState VS 需要发送给view层的viewState
+     */
+    protected fun addViewState(viewState: VS) {
+        viewModelScope.launch(Dispatchers.Main) {
+            viewStateLiveData.value = viewState
+        }
+    }
+
 
     //允许修改
     protected open val viewStateLiveData: MutableLiveData<VS> by lazy {
